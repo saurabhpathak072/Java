01c17d2e73bb90e8169f316c58d7e2f70560dcf5
Saurabh Pathak <SPathak@eagle.org>
2023-08-26 11:48:48 +0530

    Added Record Type

--- src/OOPS/Customer/Customer.java
+++ src/OOPS/Customer/Customer.java
@@ -5,17 +5,18 @@ public class Customer {
     private int credit_limit;
     private String email;
 
+    public Customer() {
+        this("Raju", 25000, "abcd@email.com");
+    }
+
+    public Customer(String name, String email) {
+        this(name, 35900, email);
+    }
+
     public Customer(String name, int credit_limit, String email) {
         this.name = name;
         this.credit_limit = credit_limit;
         this.email = email;
-    }
-    public Customer(){
-        this("Raju",25000,"abcd@email.com");
-    }
-
-    public Customer(String name,String email){
-        this(name, 35900,email);
     }
 
     public String getName() {
