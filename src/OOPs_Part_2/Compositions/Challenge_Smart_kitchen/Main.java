a897ff74bdbdcdb700da3e40fe87ca7df367c2f5
Saurabh Pathak <SPathak@eagle.org>
2023-09-23 13:08:30 +0530

    personal computer composition

---
+++ src/OOPs_Part_2/Compositions/Challenge_Smart_kitchen/Main.java
@@ -0,0 +1,4 @@
+package OOPs_Part_2.Compositions.Challenge_Smart_kitchen;
+
+public class Main {
+}
---
+++ src/OOPs_Part_2/Compositions/Main.java
@@ -0,0 +1,9 @@
+package OOPs_Part_2.Compositions;
+
+public class Main {
+//    Inheritance IS-A Relationship
+//    Composition HAS-A Relationship
+    public static void main(String[] args) {
+
+    }
+}
