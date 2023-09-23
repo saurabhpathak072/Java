79a1cf421eff755bbef1c60b1b62f1718e47c34a
saurabhpathak072 <saurabhpathak072@gmail.com>
2023-09-23 15:59:38 +0530

    {Composition} SmartKitchen Challenge

--- src/OOPs_Part_2/Compositions/Challenge_Smart_kitchen/Main.java
+++ src/OOPs_Part_2/Compositions/Challenge_Smart_kitchen/Main.java
@@ -1,4 +1,20 @@
 package OOPs_Part_2.Compositions.Challenge_Smart_kitchen;
 
 public class Main {
+    public static void main(String[] args) {
+
+        SmartKitchen kitchen = new SmartKitchen();
+
+//        kitchen.getDishWasher().setHasWorkToDo(true);
+//        kitchen.getDishWasher().doDishes();
+//
+//        kitchen.getIceBox().setHasWorkToDo(true);
+//        kitchen.getIceBox().orderFood();
+//
+//        kitchen.getBrewMaster().setHasWorkToDo(true);
+//        kitchen.getBrewMaster().brewCoffee();
+
+        kitchen.setKitchenState(true,false,true);
+        kitchen.doKitchenWork();
+    }
 }
