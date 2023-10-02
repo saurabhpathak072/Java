8cd358e8abde165764f27a9f8c77ffec24ab154e
saurabhpathak <saurabhpathak072@gmail.com>
2023-10-02 12:30:55 +0530

    {WIP} Added code for simple polymorphism

---
+++ src/OOPs_Part_2/Polymorphism/MoviesApplication/Main.java
@@ -0,0 +1,8 @@
+package OOPs_Part_2.Polymorphism.MoviesApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        Movie theMovies = new Adventure("Star Wars");
+        theMovies.watchMovies();
+    }
+}
