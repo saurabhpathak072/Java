ed5dadc5751123bebef621a37691c63d99e46b7c
Saurabh Pathak <SPathak@eagle.org>
2023-10-23 14:29:45 +0530

    Added Burger and Created burger

--- src/OOPs_Part_2/Polymorphism/BillBurgerchallenge/Main.java
+++ src/OOPs_Part_2/Polymorphism/BillBurgerchallenge/Main.java
@@ -2,12 +2,16 @@ package OOPs_Part_2.Polymorphism.BillBurgerchallenge;
 
 public class Main {
     public static void main(String[] args) {
-        Items coke = new Items("Drink","Coke",1.5);;
-        coke.printItem();
-        coke.setSize("LARGE");
-        coke.printItem();
+//        Items coke = new Items("Drink","Coke",1.5);;
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//
+//        Items avocado = new Items("Toppings","avocado",1.5);
+//        avocado.printItem();
 
-        Items avocado = new Items("Toppings","avocado",1.5);
-        avocado.printItem();
+        Burger burger = new Burger("regular",4.0);
+        burger.addToppings("Bacon","CHEESE","MAYO");
+        burger.printItem();
     }
 }
